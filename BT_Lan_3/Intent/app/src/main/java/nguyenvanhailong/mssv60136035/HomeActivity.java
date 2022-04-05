0f1524f65ea1ad7e65c80aef6c4c39f6c212b86b
Nguyễn Văn Hải Long <apolokaio2000@gmail.com>
2022-04-05 13:32:01 +0700

    Update

--- BT_Lan_3/Intent/app/src/main/java/nguyenvanhailong/mssv60136035/HomeActivity.java
+++ BT_Lan_3/Intent/app/src/main/java/nguyenvanhailong/mssv60136035/HomeActivity.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity {
@@ -16,40 +17,12 @@ public class HomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_home);
+        Intent iHome = getIntent();
+        TextView tvUserName = (TextView) findViewById(R.id.txt8);
+        ImageView anh = (ImageView) findViewById(R.id.imageView);
 
-        Button btnXacNhan = (Button) findViewById(R.id.btnOK);
-        btnXacNhan.setOnClickListener(XuLyDangNhap);
+        tvUserName.setText(String.valueOf(iHome.getStringExtra("tenNguoiDung")));
+        anh.setImageResource(R.drawable.main);
     }
-
-    View.OnClickListener XuLyDangNhap = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            EditText username = (EditText) findViewById(R.id.edtUserName);
-            EditText password = (EditText) findViewById(R.id.edtPass);
-            EditText email = (EditText) findViewById(R.id.edtEmail);
-            ImageView image = (ImageView) findViewById(R.id.imageView);
-
-            String x = username.getText().toString();
-            String y = password.getText().toString();
-            String z = email.getText().toString();
-            String user ="longfull2000";
-            String pass ="123456";
-            String mail="nguyenvanhailong2000@gmail.com";
-
-
-            if ((x.equals(user)) && (y.equals(pass)) && (z.equals(mail))) {
-
-                Toast.makeText(HomeActivity.this, "Đăng nhập thành công..", Toast.LENGTH_LONG).show();
-                Intent iHome = new Intent(HomeActivity.this,HomeActivity.class);
-                iHome.putExtra("tenNguoiDung",x);
-                startActivity(iHome);
-                image.setImageResource(R.drawable.main);
-            } else {
-                Toast.makeText(HomeActivity.this, "Bạn đã nhập sai thông tin, hãy nhập lại !", Toast.LENGTH_LONG).show();
-            }
-        }
-    };
-
-
 }
