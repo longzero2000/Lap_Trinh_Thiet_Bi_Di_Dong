f12d9212f9cc0dc8440963031bfc5d0407d7bdc5
Nguyễn Văn Hải Long <apolokaio2000@gmail.com>
2022-04-08 12:48:28 +0700

    update

--- Bai_Lam_Them/Chuyen_giong_noi_thanh_van_ban/app/src/main/java/com/nguyenvanhailong/speechtotext/MainActivity.java
+++ Bai_Lam_Them/Chuyen_giong_noi_thanh_van_ban/app/src/main/java/com/nguyenvanhailong/speechtotext/MainActivity.java
@@ -2,13 +2,39 @@ package com.nguyenvanhailong.speechtotext;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognitionService;
+import android.speech.RecognizerIntent;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Views
+    TextView mTextTv;
+    ImageButton mBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTextTv = findViewById(R.id.textTv);
+        mBtn = findViewById(R.id.btn);
+
+        //Sự kiện click Btn
+        mBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                speak();
+            }
+        });
+    }
+    private void speak(){
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+
     }
 }
