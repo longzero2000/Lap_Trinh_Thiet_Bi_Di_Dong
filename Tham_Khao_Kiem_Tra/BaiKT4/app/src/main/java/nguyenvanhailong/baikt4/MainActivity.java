be77090bdfbb68812c73601a04e56417d260dd01
Nguyễn Văn Hải Long <apolokaio2000@gmail.com>
2022-04-12 01:25:57 +0700

    Update

--- Tham_Khao_Kiem_Tra/BaiKT4/app/src/main/java/nguyenvanhailong/baikt4/MainActivity.java
+++ Tham_Khao_Kiem_Tra/BaiKT4/app/src/main/java/nguyenvanhailong/baikt4/MainActivity.java
@@ -3,8 +3,19 @@ package nguyenvanhailong.baikt4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+    int[] images = {R.drawable.pet, R.drawable.pet, R.drawable.pet, R.drawable.rabbit, R.drawable.pet, R.drawable.pet};
+
+    String[] name = {"Octopus", "Pig", "Sheep", "Rabbit", "Snake", "Spider"};
+
+    String[] info = {"8 tentacled monster", "Delicious in rolls", "Great for jumpers", "Nice in a stew", "Great for shoes", "Poison"};
+
+    ListView lView;
+
+    ListAdapter lAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
