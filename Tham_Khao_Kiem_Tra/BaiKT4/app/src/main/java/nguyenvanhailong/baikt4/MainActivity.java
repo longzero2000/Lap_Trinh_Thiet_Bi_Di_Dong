9d619a5cafcc0a7eef8af1fcfa04e4191ef60758
Nguyễn Văn Hải Long <apolokaio2000@gmail.com>
2022-04-12 02:04:47 +0700

    Update

--- Tham_Khao_Kiem_Tra/BaiKT4/app/src/main/java/nguyenvanhailong/baikt4/MainActivity.java
+++ Tham_Khao_Kiem_Tra/BaiKT4/app/src/main/java/nguyenvanhailong/baikt4/MainActivity.java
@@ -3,11 +3,14 @@ package nguyenvanhailong.baikt4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    int[] images = {R.drawable.pet, R.drawable.pet, R.drawable.pet, R.drawable.rabbit, R.drawable.pet, R.drawable.pet};
+    int[] images = {R.drawable.octopus, R.drawable.pig, R.drawable.sheep, R.drawable.rabbit, R.drawable.snake, R.drawable.spider};
 
     String[] name = {"Octopus", "Pig", "Sheep", "Rabbit", "Snake", "Spider"};
 
@@ -21,5 +24,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        lView = (ListView) findViewById(R.id.animal);
+
+        lAdapter = new nguyenvanhailong.baikt4.ListAdapter(MainActivity.this,name,info,images);
+
+        lView.setAdapter(lAdapter);
+
+        lView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                Toast.makeText(MainActivity.this, name[i]+" "+info[i], Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
 }
