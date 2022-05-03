ff6891c762df894ae2b982c339c87ba78743de1c
Nguyễn Văn Hải Long <apolokaio2000@gmail.com>
2022-05-03 13:35:06 +0700

    Update

---
+++ Bai_Lam_Them/Temperature_Conversion/app/src/main/java/com/nguyenvanhailong/temperature_conversion/MainActivity.java
@@ -0,0 +1,19 @@
+package com.nguyenvanhailong.temperature_conversion;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button C,F;
+    EditText NumberC;
+    EditText NumberF;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
